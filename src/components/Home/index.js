e73c56b9b4206aeb1cab1769bf568d2090553a4c
raiankit12 <ankit789101gmail.com>
2024-10-13 02:59:09 +0530

    portfio changes

---
+++ src/components/Home/index.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import "./index.scss";
+import Logo from "./Logo";
+import AnimatedLetters from "../AnimatedLetters";
+import Loader from "react-loaders";
+const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["n", "k", "i", "t", "s"];
+  const jobArray = [
+    "F",
+    "r",
+    "o",
+    "n",
+    "t",
+    " ",
+    "E",
+    "n",
+    "d",
+    " ",
+    "D",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+    ".",
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m,</span>
+            <img src={logo} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>FrontEnd Developer / Javascript / ReactJS</h2>
+          <Link to="/contact" className="flat-button">
+            CONNECT WITH ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,99 @@
+import "./index.scss";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faClose,
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import logoM from "../../assets/images/logoM.png";
+import logoSubtitle from "../../assets/images/logoSubtitle.png";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={logoM} alt="logo" />
+        <img className="sub-logo" src={logoSubtitle} alt="Ankit" />
+      </Link>
+      <nav className={showNav ? "mobile-show" : ""}>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/ankit-kumar-rai-217b72179/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/raiankit12"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  );
+};
+
+export default Sidebar;
